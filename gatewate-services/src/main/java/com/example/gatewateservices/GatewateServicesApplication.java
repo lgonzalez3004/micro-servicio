0a166b8ae704aa92840117daef781a5718ac23ab
lgonzalez3004 <luisant.gonzalezdj@gmail.com>
2022-12-13 02:32:05 +0100

    gateway

---
+++ gatewate-services/src/main/java/com/example/gatewateservices/GatewateServicesApplication.java
@@ -0,0 +1,15 @@
+package com.example.gatewateservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewateServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewateServicesApplication.class, args);
+	}
+
+}
---
+++ gatewate-services/src/test/java/com/example/gatewateservices/GatewateServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewateservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewateServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
